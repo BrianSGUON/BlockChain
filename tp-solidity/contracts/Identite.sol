f1e5b4af8c11cd1a523e4971081d7e2495c046bf
Brian Sguon <bsguon@gmail.com>
2026-04-09 09:56:40 +0200

    identite

---
+++ tp-solidity/contracts/Identite.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.20;
+
+contract Identite {
+    string public nom; 
+    uint8 public age; 
+    address public portefeuille; 
+    bool public estMajeur; 
+    uint256 public solde; 
+    bytes32 public identifiantUnique; 
+}
+
