a169de2f99ef88f33d2b0a530b95a404a3700beb
BrianSGUON <bsguon@gmail.com>
2026-04-15 09:12:54 +0200

    constants

---
+++ dappvote-frontend/src/constants/contract.js
@@ -0,0 +1,18 @@
+
+
+export const CONTRACT_ABI = [
+  "constructor(string[] _candidateNames)",
+  "function addVoter(address _voter)",
+  "function startVoting()",
+  "function vote(uint256 _candidateIndex)",
+  "function candidates(uint256) view returns (string name, uint256 voteCount)",
+  "function getCandidatesCount() view returns (uint256)",
+  "function getWinner() view returns (string winnerName)",
+  "function owner() view returns (address)",
+  "function votingOpen() view returns (bool)",
+  "function whitelist(address) view returns (bool)",
+  "function hasVoted(address) view returns (bool)",
+  "event VoterAdded(address indexed voter)",
+  "event VotingStarted()",
+  "event VoteCast(address indexed voter, uint256 indexed candidateIndex)"
+];
